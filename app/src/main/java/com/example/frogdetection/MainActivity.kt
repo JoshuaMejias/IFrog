de97047795eec7a7af1b987bb725b6842e7c6af8
JoshuaMejias <joshua.mejias@bisu.edu.ph>
2025-09-02 21:50:57 +0800

    initial

--- app/src/main/java/com/example/frogdetection/MainActivity.kt
+++ app/src/main/java/com/example/frogdetection/MainActivity.kt
@@ -20,6 +20,7 @@ import com.example.frogdetection.screens.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContent {
             iFrogTheme {
